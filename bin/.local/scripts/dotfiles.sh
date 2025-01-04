f02806523d65fc226bf1eb2b3d76c77741605da2
EdgeLimits <edgars.kru@gmail.com>
2025-01-04 20:10:03 +0100

    chore: update dotfiles.sh sessions

--- bin/.local/scripts/dotfiles.sh
+++ bin/.local/scripts/dotfiles.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
+SESSION_NAME="dotfiles"
+GITHUB_REPO="git@github.com:EdgeLimits/.dotfiles.git"
+DIRECTORY="$HOME/.dotfiles"
+
+PERSONAL_GITHUB_REPO="git@github.com:EdgeLimits/.dotfiles-personal.git"
+PERSONAL_DIRECTORY="$HOME/.dotfiles-personal"
+
+WORK_GITHUB_REPO="git@github.com:EdgeLimits/.dotfiles-work.git"
+WORK_DIRECTORY="$HOME/.dotfiles-work"
+
 UPDATE_FLAG=0
 if [[ " $@ " == *" --update "* ]]; then
   UPDATE_FLAG=1
 fi
 
-cd ~/.dotfiles
+if [ ! -d "$DIRECTORY" ]; then
+  git clone $GITHUB_REPO "$DIRECTORY"
+fi
+
+if [[ "$DOTFILES_ENABLE_PERSONAL" == "True" ]] && [ ! -d "$PERSONAL_DIRECTORY" ]; then
+  git clone $PERSONAL_GITHUB_REPO "$PERSONAL_DIRECTORY"
+fi
+
+if [[ "$DOTFILES_ENABLE_WORK" == "True" ]] && [ ! -d "$WORK_DIRECTORY" ]; then
+  git clone $WORK_GITHUB_REPO "$WORK_DIRECTORY"
+fi
+
+cd $DIRECTORY
 source .env
 
 if ! pgrep -x "tmux" >/dev/null; then
@@ -13,7 +35,6 @@ if ! pgrep -x "tmux" >/dev/null; then
   tmux start-server
 fi
 
-SESSION_NAME="dotfiles"
 if ! tmux has-session -t $SESSION_NAME 2>/dev/null; then
   echo "Creating new session: $SESSION_NAME"
 
@@ -25,29 +46,28 @@ if ! tmux has-session -t $SESSION_NAME 2>/dev/null; then
   if [[ "$DOTFILES_ENABLE_PERSONAL" == "True" ]]; then
     tmux new-window -t $SESSION_NAME -n personal-source
     sleep 0.5
-    tmux send-keys -t $SESSION_NAME:personal-source "cd personal" C-m
+    tmux send-keys -t $SESSION_NAME:personal-source "cd $PERSONAL_DIRECTORY" C-m
     tmux send-keys -t $SESSION_NAME:personal-source "nvim ." C-m
 
     tmux new-window -t $SESSION_NAME -n personal-shell
     sleep 0.5
-    tmux send-keys -t $SESSION_NAME:personal-shell "cd personal" C-m
+    tmux send-keys -t $SESSION_NAME:personal-shell "cd $PERSONAL_DIRECTORY && clear" C-m
   fi
   if [[ "$DOTFILES_ENABLE_WORK" == "True" ]]; then
     tmux new-window -t $SESSION_NAME -n work-source
     sleep 0.5
-    tmux send-keys -t $SESSION_NAME:work-source "cd work" C-m
+    tmux send-keys -t $SESSION_NAME:work-source "cd $WORK_DIRECTORY" C-m
     tmux send-keys -t $SESSION_NAME:work-source "nvim ." C-m
 
     tmux new-window -t $SESSION_NAME -n work-shell
     sleep 0.5
-    tmux send-keys -t $SESSION_NAME:work-shell "cd work" C-m
+    tmux send-keys -t $SESSION_NAME:work-shell "cd $WORK_DIRECTORY && clear" C-m
   fi
 
-  tmux select-window -t :1
 fi
 
 if [[ "$UPDATE_FLAG" == "1" ]]; then
-  tmux send-keys -t $SESSION_NAME:shell "git pull" C-m
+  tmux send-keys -t $SESSION_NAME:shell "git pull && git submodule update --remote" C-m
 
   if [[ "$DOTFILES_ENABLE_PERSONAL" == "True" ]]; then
     tmux send-keys -t $SESSION_NAME:personal-shell "git pull" C-m
@@ -64,4 +84,4 @@ else
   tmux switch-client -t $SESSION_NAME
 fi
 
-exit 0
+tmux select-window -t :1
