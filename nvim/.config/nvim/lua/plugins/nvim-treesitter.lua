7ea00fae6312383eb3d0d4c83e3b13c6e6ab279a
EdgeLimits <hello@edgelimits.com>
2026-04-10 12:07:31 +0200

    chore: treesitter version lock

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,34 +1,27 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
+  tag = "v0.9.3",
   build = ":TSUpdate",
   config = function()
-    local ts = require("nvim-treesitter")
-    local parsers = {
-      "bash",
-      "astro",
-      "css",
-      "dockerfile",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "scss",
-      "typescript",
-      "svelte",
-    }
-
-    for _, parser in ipairs(parsers) do
-      ts.install(parser)
-    end
-
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = parsers,
-      callback = function()
-        pcall(vim.treesitter.start)
-      end,
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "bash",
+        "astro",
+        "css",
+        "dockerfile",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "scss",
+        "typescript",
+        "svelte",
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
     })
   end,
 }
